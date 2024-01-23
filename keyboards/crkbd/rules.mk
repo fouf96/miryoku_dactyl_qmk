1b297f6944b240276af8557eaa2ce7a6b2e14e03
Tworck <59090867+Tworck@users.noreply.github.com>
2024-01-23 22:48:33 +0100

    try our r2g config and change led pin definition

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -7,17 +7,17 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
 
 # if firmware size over limit, try this option
 # LTO_ENABLE = yes
 
-DEFAULT_FOLDER = crkbd/rev1
+DEFAULT_FOLDER = crkbd/r2g
 
 RGBLIGHT_SUPPORTED = yes
 RGB_MATRIX_SUPPORTED = yes
